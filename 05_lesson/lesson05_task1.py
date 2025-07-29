69f78bb8cee8e07a85ce18492b71301d2ae1cc64
Alexandra21-crush <arcobalenox@yandex.ru>
2025-07-29 07:38:29 +0300

    сделана 5 дз

---
+++ 05_lesson/lesson05_task1.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/classattr")
+driver.find_element(
+    By.XPATH,
+    (
+        "//button[contains(concat(' ', normalize-space(@class), ' '), "
+        "' btn-primary ')]"
+    )
+).click()
+time.sleep(5)
+
+# driver.quit()
---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/dynamicid")
+time.sleep(2)
+driver.find_element(
+    By.XPATH,
+    "//button[contains(concat(' ', normalize-space(@class)), ' btn-primary')]"
+).click()
+time.sleep(5)
